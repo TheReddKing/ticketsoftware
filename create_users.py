0f2617311ff992ec0deef59b51e007ece51f933b
Kevin Fang <reddacc@gmail.com>
2019-09-22 16:25:01 -0400

    update user creation doc and template

--- create_users.py
+++ create_users.py
@@ -2,17 +2,12 @@ from server.app import app, db
 from server.models.ticket import Ticket
 from server.models.user import User
 
-# user = User(1, "test account", False)
-# db.session.add(user)
-# db.session.commit()
-
-user = User(0, "MIT CSC", True)
-db.session.add(user)
-print(user.token)
-db.session.commit()
-
-user = User(0, "Harvard CSA", True)
-db.session.add(user)
-print(user.token)
-db.session.commit()
-
+print("Run python create_users.py -i to add users");
+print("Run createUser(ticket_limit, name, is_admin)");
+print("ticket_limit == 0 will give the account unlimited tickets!");
+def createUser(tickets, name, is_admin):
+    user = User(tickets, name, is_admin)
+    db.session.add(user)
+    db.session.commit()
+    print("CREATED USER " + user.name)
+    print("TOKEN: " + user.token)
